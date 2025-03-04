910c08e7a3d9aa58f41383901af045ec3e64327e
Graham Clifford <gclifford@modalic.com>
2025-03-04 18:17:56 -0500

    put everything into release mode from debug and reverted GetPCLMap

--- build.sh
+++ build.sh
@@ -1,30 +1,27 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
-trash-put build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
-trash-put build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 cd ../../Sophus
 
 echo "Configuring and building Thirdparty/Sophus ..."
 
-trash-put build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 cd ../../../
@@ -37,8 +34,7 @@ cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
 
-trash-put build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
-make -j8
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j4

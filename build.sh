0f29ad5f563269a13109d81eb886916cc5960136
Graham Clifford <gjcliff@gmail.com>
2026-08-18 22:22:07 -0400

    modified build.sh
    might come back to this later, works lovely in the docker container
    though

--- build.sh
+++ build.sh
@@ -1,44 +1,35 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
-
 cd Thirdparty/DBoW2
-rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
-cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
-
-rm -rf build
+cd ../../g2o
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
-cd ../../Sophus
-
 echo "Configuring and building Thirdparty/Sophus ..."
-
-rm -rf build
+cd ../../Sophus
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF
 make -j
 
-cd ../../../
 
 echo "Uncompress vocabulary ..."
-
+cd ../../../
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
-cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
-
+cd ..
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j16
